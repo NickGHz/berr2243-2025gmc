4a46da1a2bcc8170ac7eac16e66bd948b451f7e7
goh ming chen <b122420101@student.utem.edu.my>
2025-10-28 01:05:17 +0800

    Exercise2

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const { MongoClient } = require('mongodb');
 
+const drivers = [
+    {
+        name: "John Doe",
+        vehicleType: "Sedan",
+        isAvalable: true,
+        rating: 4.8
+    },
+    {
+        name: "Alice Smith",
+        vehicleType: "SUV",
+        isAvalable: false,
+        rating: 4.5
+    }
+];
+
+console.log(drivers);
+
 async function main() {
     const uri = "mongodb://localhost:27017";
     const client = new MongoClient(uri);
@@ -10,12 +27,34 @@ async function main() {
 
         const db = client.db("testDB");
         const collection = db.collection("users");
+        const driversCollection = db.collection("drivers");
+
+        drivers.forEach(async (driver) => {
+            const result = await driversCollection.insertOne(driver);
+            console.log(`New driver created with result; ${result}`);
+        });
+
+        const updatedResult = await db.collection('drivers').updateOne(
+            { name: "John Doe" },   
+            { $inc: { rating: 0.1 } }
+        );
+        console.log(`Driver updated with result: ${updatedResult}`);
+
+        const availableDrivers = await db.collection('drivers').find({
+            isAvalable: true, 
+            rating: { $gte: 4.5 }
+        }).toArray();
+        console.log("Available drivers:", availableDrivers);
 
         await collection.insertOne({ name: "Goh Ming Chen", age: 21 });
         console.log("Document inserted!");
 
         const result = await collection.findOne({ name: "Goh Ming Chen" });
         console.log("Query result:", result);
+
+        // const deleteResult = await db.collection(`drivers`).deleteOne({ isAvalable: false });
+        // console.log(`Driver deleted with result: ${deleteResult}`);
+
     } catch (err) {
         console.error("Error:", err);
     } finally {
